45aa55aee7b04b83d0c71a06ec91678a50cf838e
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-08-15 14:04:56 -0400

    Create 03-CreateListInSplitCardFormat.py
    Use after 02-ParseList-SaveViableNumbers.py
    
    Takes a list of numbers made up of 16 digits each and formats them like a credit card.
    
    For example,  1111000011110000 results in 1111 0000 1111 0000
    
    For informational purposes only.

---
+++ 03-CreateListInSplitCardFormat.py
@@ -0,0 +1,32 @@
+'''
+Use after 02-ParseList-SaveViableNumbers.py
+Takes a list of numbers made up of 16 digits each and formats them like a credit card.
+For example,  1111000011110000 results in 1111 0000 1111 0000
+For informational purposes only.
+'''
+
+def format_numbers(input_file, output_file):
+    with open(input_file, "r") as f:
+        numbers = f.readlines()
+
+    formatted_numbers = []
+
+    for number in numbers:
+        number = number.strip()
+
+        # Split the number into groups of 4 digits
+        groups = [number[i:i+4] for i in range(0, len(number), 4)]
+
+        # Join the groups with spaces, except the last group
+        formatted_number = " ".join(groups[:-1]) + groups[-1]
+
+        formatted_numbers.append(formatted_number)
+
+    with open(output_file, "w") as f:
+        f.write("\n".join(formatted_numbers))
+
+if __name__ == "__main__":
+    input_file = "viablenumbers.txt"  # Replace with your input file path
+    output_file = "formatted_numbers.txt"  # Replace with your output file path
+
+    format_numbers(input_file, output_file)
